37ce329e56f136b78cb38516b20877df54f188fb
Yarom Vargas <yaromvp@gmail.com>
2024-10-13 21:12:11 +0000

    Solo SeconsCounter, sin bonus :( - yaromvp

--- src/js/component/Timer.jsx
+++ src/js/component/Timer.jsx
@@ -31,9 +31,9 @@ export const Timer = (props) => {
 
     return (
         <div className="d-flex justify-content-evenly p-2">
-            <button type="button" className={`btn btn-${props.start ? "secondary" : "primary"} mx-1" ${props.start ? "disabled" : ""}`} onClick={clickStart}>Start</button>
-            <button type="button" className={`btn btn-${props.stop ? "secondary" : "danger"} mx-1 ${props.stop ? "disabled" : ""}`} onClick={clickStop}>Stop</button>
-            <button type="button" className="btn btn-success mx-1" onClick={clickRestart}>Restart</button>
+            <button className={`btn btn-${props.start ? "secondary" : "primary"} mx-1 ${props.start ? "disabled" : ""}`} onClick={clickStart}>Start</button>
+            <button className={`btn btn-${props.stop ? "secondary" : "danger"} mx-1 ${props.stop ? "disabled" : ""}`} onClick={clickStop}>Stop</button>
+            <button className="btn btn-success mx-1" onClick={clickRestart}>Restart</button>
         </div>
     )
 
